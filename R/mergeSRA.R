024c56b613fc67740357957e444dbcf8385357e4
Thomas Nicholson <thomasnicholson@dn-wlan-159-026.sns.otago.ac.nz>
2020-01-31 12:13:47 +1300

    mergeSRA function needed to be updated to consider the final row in the loop

--- R/mergeSRA.R
+++ R/mergeSRA.R
@@ -82,7 +82,7 @@ mergeSRA <- function(ncRNAgff, gff1, gff2, time.it = T, quiet = F, filenum1 = "1
   # Main loop ---------------------------------------------------------------
 
 
-  for(i in 1:(nrow(ncRNAgff) - 1)){
+  for(i in 1:(nrow(ncRNAgff))){
 
     if(quiet ==F){
     printRemaining(i <- i, length = nrow(ncRNAgff) - 1, increment = 5)
@@ -205,6 +205,116 @@ mergeSRA <- function(ncRNAgff, gff1, gff2, time.it = T, quiet = F, filenum1 = "1
     }
 
     ##check if the current end value overlaps with the next starting value and update the end value if it does
+    if(i == nrow(ncRNAgff)){
+
+        ##check if the subsequent feature was contained within the first feature
+        if(ncRNAgff$end[start_i] < end_val){
+          prop_val <- (ncRNAgff$end[start_i] - ncRNAgff$start[i])/(end_val - start_val)
+        }else{
+          prop_val <- 1
+        }
+
+        ##check if there was more than one feature overlapping
+        feature_matched <- ifelse(length(unique(ncRNAgff$file_id[start_i:i])) > 1, T, F)
+
+
+        #select the current feature from the dataframe
+        idRows <- ncRNAgff[start_i:i,]
+
+        ##keep the ids from rows of the dataframe that match the filenum (file name)
+        ##The purpose of this is to keep track of each original file and row number for each feature as continued merging is done
+        id1_val <- paste(idRows$id[idRows$file_id == filenum1], collapse = "-")
+        id2_val <- paste(idRows$id[idRows$file_id == filenum2], collapse = "-")
+
+        ##assign each missing file as an id of filenum_0
+        ##this needs to be done for each individual file so that it appears as filenum1_0-filenum2_0 not filenum1-filenum2_0
+        ##as this will make tracking which files contributed easier
+        if(id1_val == ""){
+          id_vals <- unlist(strsplit(filenum1, "-"))
+          id_vals <- paste(id_vals, "_0", sep = "")
+          id1_val <- paste(id_vals, collapse = "-")
+        }
+
+        if(id2_val == ""){
+          id_vals <- unlist(strsplit(filenum2, "-"))
+          id_vals <- paste(id_vals, "_0", sep = "")
+          id2_val <- paste(id_vals, collapse = "-")
+        }
+
+        ##take the current set value and if one does not exist then set to 1
+        set_val_1 <- idRows$set_val[idRows$file_id == filenum1]
+        set_val_2 <- idRows$set_val[idRows$file_id == filenum2]
+
+        set_val_1 <- interset_all(set_val_1)
+        set_val_2 <- interset_all(set_val_2)
+
+
+        if(set_val_1 == ""){
+          set_val_1 <- "0"
+        }
+        if(set_val_2 == ""){
+          set_val_2 <- "0"
+        }
+
+
+        ##Number of features
+        ##Combine each set of ids and get a unique list of each id available
+        id_val <- paste(id1_val, id2_val, sep = "-")
+        idList <- unique(unlist(strsplit(id_val, "-")))
+        ##loop through each ID and check if it is real or a place holder. All real IDs are 1 and placeholders are 0
+        for(j in 1:length(idList)){
+          number <- unlist(strsplit(as.character(idList[j]), "_"))
+          if(length(number)  < 3){
+            idList[j] <- as.numeric(0)
+          }else{
+            number <- number[3]
+            if(number == "0"){
+              idList[j] <- as.numeric(0)
+            }else{
+              idList[j] <- as.numeric(1)
+            }
+          }
+        }
+        number_of_features <- sum(as.numeric(idList))
+
+        ##get the set values from old features
+        if(initial_data == F){
+          idSetValues <- ncRNAgff[start_i:i,c(17:(ncol(ncRNAgff)))]
+          for(j in 1:ncol(idSetValues)){
+            idSetValues[1,j] <- interset_all(idSetValues[idSetValues[,j]!= "0",j])
+
+          }
+          idSetValues <- idSetValues[1,]
+        }
+
+        tmp <- data.frame(sequence = ncRNAgff$sequence[i],
+                          feature = ncRNAgff$feature[i],
+                          start = start_val, end = end_val,
+                          strand = ncRNAgff$strand[i],
+                          file_names = paste(unique(ncRNAgff$file_id[start_i:i]), collapse = ","),
+                          row_numbers = as.character(paste(c(start_i:i), collapse = ",")),
+                          prop_overlap = prop_val,
+                          feature_match = feature_matched,
+                          number_of_features = number_of_features,
+                          score = as.character(ncRNAgff$score[i]),
+                          new_feature = !(F %in% ncRNAgff$new_feature[start_i:i]),
+                          number_of_rnaseq_files = sum(as.integer(ncRNAgff$number_of_rnaseq_files[start_i:i])),
+                          id1 = id1_val,
+                          id2 = id2_val,
+                          set_val_1 = as.character(set_val_1),
+                          set_val_2 = as.character(set_val_2),
+                          file_id = paste(filenum1, "-", filenum2, sep = ""),
+                          stringsAsFactors = F)
+        if(initial_data == F){
+          tmp <- tmp %>% bind_cols(idSetValues)
+        }
+        mergedDat <- mergedDat%>%bind_rows(tmp)
+
+        ##feature has been added so start again
+        current_feature <- F
+        new_feature <- F
+
+    }else{
     if(end_val > ncRNAgff$start[i + 1] & ncRNAgff$strand[i] == ncRNAgff$strand[i+1]){
       if(ncRNAgff$end[i + 1] > end_val){
         end_val <- ncRNAgff$end[i + 1]
@@ -319,6 +429,7 @@ mergeSRA <- function(ncRNAgff, gff1, gff2, time.it = T, quiet = F, filenum1 = "1
       ##feature has been added so start again
       current_feature <- F
       new_feature <- F
+    }
     }
   }
 
